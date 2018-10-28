01fccccd50424eb5abe72f344aad3f32ab619af1
Hiep Nguyen Ngoc <ngochiep9523@gmail.com>
2018-10-28 11:44:30 +0700

    init firebase

--- android/app/src/main/java/com/solotravellerapp/mytravelapp/MainApplication.java
+++ android/app/src/main/java/com/solotravellerapp/mytravelapp/MainApplication.java
@@ -1,17 +1,31 @@
-package com.solo;
+package com.solotravellerapp.mytravelapp;
+
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.solotravellerapp.mytravelapp.BuildConfig;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +36,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNFirebasePackage(),
+            new FBSDKPackage(mCallbackManager),
+            new RNFirebaseAuthPackage()
       );
     }
 
